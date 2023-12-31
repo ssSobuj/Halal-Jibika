b7151422d49990116ee3f7cf4335c9aa4a9d6a81
ssSobuj <mdsobujridoy@gmail.com>
2023-12-31 20:34:01 +0600

    nabver digign again

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -20,60 +20,12 @@ export default function Header() {
   };
   return (
     <>
-      {/* <div className="header-wrap">
-        <nav>
-          <div className="logo">
-            <img src="/public/logo-removebg.png" alt="" />
-          </div>
-          <div className="main-menu">
-            <ul>
-              <li>
-                <NavLink className="nav-link" to={""}>
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to={"/about"}>
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to={"./contact"}>
-                  Contact
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to={"./jobs"}>
-                  Jobs
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to={"./feverite"}>
-                  Favorite
-                </NavLink>
-              </li>
-              {user ? (
-                <li>
-                  <Link onClick={logOut} className="nav-link">
-                    Log Out
-                  </Link>
-                </li>
-              ) : (
-                <li>
-                  <NavLink className="nav-link" to={"./login"}>
-                    Log in
-                  </NavLink>
-                </li>
-              )}
-              <li>{user?.photoURL && <img src={user?.photoURL} alt="" />}</li>
-            </ul>
-          </div>
-        </nav>
-      </div> */}
       <nav className="navbar">
         <div className="container">
           <div className="logo">
-            <img src="/public/logo-removebg.png" alt="" />
+            <Link>HALAL JIBIKA</Link>
+            {/* <img src="/public/logo-removebg.png" alt="" />
+             */}
           </div>
           <div className="menu-icon" onClick={handleShowNavbar}>
             <AiOutlineMenuFold />
@@ -95,23 +47,27 @@ export default function Header() {
               <li onClick={handleShowNavbar}>
                 <NavLink to="/contact">Contact</NavLink>
               </li>
-              {user ? (
-                <li>
-                  <Link onClick={logOut}>Sign Out</Link>
-                </li>
-              ) : (
-                <li>
-                  <NavLink to={"./login"}>Sign In</NavLink>
-                </li>
-              )}
-              <li className="user-logo">
-                {user?.photoURL && (
+              <div className="signup-signin">
+                {user ? (
                   <>
-                    <img src={user?.photoURL} alt="" />
-                    <h5>{user?.displayName}</h5>
+                    <li className="last-li">
+                      <Link onClick={logOut}>Sign Out</Link>
+                    </li>
+                    <li className="user-logo">
+                      {user?.photoURL && <img src={user?.photoURL} alt="" />}
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="last-li">
+                      <NavLink to={"./login"}>Sign In</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to={"./singup"}>Sign Up</NavLink>
+                    </li>
                   </>
                 )}
-              </li>
+              </div>
             </ul>
           </div>
         </div>
