340f1ca06e91d464b3cd6fb79fcad9674616658a
ssSobuj <mdsobujridoy@gmail.com>
2024-01-21 17:14:56 +0600

    dising loading state

--- src/componets/loaading/Loading.jsx
+++ src/componets/loaading/Loading.jsx
@@ -2,16 +2,25 @@ import { RotatingLines } from "react-loader-spinner";
 
 export default function Loading() {
   return (
-    <RotatingLines
-      visible={true}
-      height="96"
-      width="96"
-      color="green"
-      strokeWidth="5"
-      animationDuration="0.75"
-      ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="green"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 }
